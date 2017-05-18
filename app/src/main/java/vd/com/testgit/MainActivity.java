bcd2445ee893c403d8e6613d2a3775daff81329f
Vyacheslav Dulnev <dulnev@gmail.com>
2017-05-18 15:47:26 +0300

    2

--- app/src/main/java/vd/com/testgit/MainActivity.java
+++ app/src/main/java/vd/com/testgit/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //branch master 1 change
+        //branch master 2 change
     }
 }
